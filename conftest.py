6a13f680dd9ab3b4e21e0e6b82a559c56b717f81
patricioandradeh@gmail.com <patricioandradeh@gmail.com>
2025-08-10 17:13:29 -0400

    Changes conftest

--- conftest.py
+++ conftest.py
@@ -1,8 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def driver():
-    with webdriver.Chrome() as driver:
-        yield driver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
