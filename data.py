1b05ef0b936bf7239f0c31312ce2b08494882bc2
patricioandradeh@gmail.com <patricioandradeh@gmail.com>
2025-10-25 18:53:27 -0300

    modify dynamic url

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://cnt-27f136e0-893d-4793-a27a-2bd77ecfb446.containerhub.tripleten-services.com/".strip()
+BASE_URL = "https://cnt-28df270e-3711-40ea-9c37-44a81d0f2671.containerhub.tripleten-services.com/".strip()
 class UrbanRoutesData:
     ADDRESS_FROM = "East 2nd Street, 601"
     TO_ADDRESS = "1300 1st St"
