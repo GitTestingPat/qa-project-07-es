726b6c99e41c6b9e4d3a6cfb83618f4d84572621
patricioandradeh@gmail.com <patricioandradeh@gmail.com>
2025-11-15 20:13:40 -0300

    Configurar variables de entorno para URLs dinámicas

--- data.py
+++ data.py
@@ -1,4 +1,11 @@
-BASE_URL = "https://cnt-1a4a6753-9a03-40ce-b90f-ef255ccdad73.containerhub.tripleten-services.com/".strip()
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# URL base - viene del archivo .env
+BASE_URL = os.getenv('BASE_URL', 'https://qa.urban-routes.com')
+
 class UrbanRoutesData:
     ADDRESS_FROM = "East 2nd Street, 601"
     TO_ADDRESS = "1300 1st St"
@@ -7,3 +14,4 @@ class UrbanRoutesData:
     PHONE_NUMBER = "+12312312312"
     CARD_NUMBER = "1234 0000 4321 1234"
     CARD_CODE = "123"
+    DRIVER_MESSAGE = "Por favor, llámame cuando llegues."
